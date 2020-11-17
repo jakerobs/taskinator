9f7dd5e8cc7bbbe9270d7a250dd9ec598380f64c
Jacob Hilton <jakehilton@jacobs-mbp.lan>
2020-11-17 10:05:01 -0700

    added eventlistener to input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var taskToDoEl = document.querySelector("#tasks-to-do");
-var createTaskHandlter = function() {
+var createTaskHandlter = function(event) {
+    event.preventDefault();
     var listItemEL = document.createElement("li");
     listItemEL.className = "task-item"; 
     listItemEL.textContent = "This is a new task.";
     taskToDoEl.appendChild(listItemEL);
+    console.log(event);
 }
 
-buttonEl.addEventListener("click", createTaskHandlter)
+formEl.addEventListener("submit", createTaskHandlter)
