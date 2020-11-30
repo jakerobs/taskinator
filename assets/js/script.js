23671bde182afbe502daddcf36c62a7297105903
Jake Hilton <jakerscholes@gmail.com>
2020-11-30 02:14:50 -0700

    redid the whole color scheme

--- assets/js/script.js
+++ assets/js/script.js
@@ -210,7 +210,7 @@ var dropZoneDragHandler = function(event) {
     var taskListEl = event.target.closest(".task-list");
     if (taskListEl) {
         event.preventDefault();
-        taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
+        taskListEl.setAttribute("style", "background: rgba(255, 255, 255, .2); border-style: dashed;");
     }
 }
 
